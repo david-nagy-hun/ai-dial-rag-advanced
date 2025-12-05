a84d959afd3571fafcb468ed98022b778c5896b3
david_nagy6 <david_nagy6@epam.com>
2025-12-05 11:12:08 +0100

    solution

--- task/app.py
+++ task/app.py
@@ -6,34 +6,82 @@ from task.models.conversation import Conversation
 from task.models.message import Message
 from task.models.role import Role
 
+SYSTEM_PROMPT = """You are a RAG-powered assistant that assists users with their questions about microwave usage.
 
-#TODO:
-# Create system prompt with info that it is RAG powered assistant.
-# Explain user message structure (firstly will be provided RAG context and the user question).
-# Provide instructions that LLM should use RAG Context when answer on User Question, will restrict LLM to answer
-# questions that are not related microwave usage, not related to context or out of history scope
-SYSTEM_PROMPT = """
+## Structure of User message:
+`RAG CONTEXT` - Retrieved documents relevant to the query.
+`USER QUESTION` - The user's actual question.
+
+## Instructions:
+- Use information from `RAG CONTEXT` as context when answering the `USER QUESTION`.
+- Cite specific sources when using information from the context.
+- Answer ONLY based on conversation history and RAG context.
+- If no relevant information exists in `RAG CONTEXT` or conversation history, state that you cannot answer the question.
 """
 
-#TODO:
-# Provide structured system prompt, with RAG Context and User Question sections.
-USER_PROMPT = """
-"""
+USER_PROMPT = """##RAG CONTEXT:
+{context}
 
 
-#TODO:
-# - create embeddings client with 'text-embedding-3-small-1' model
-# - create chat completion client
-# - create text processor, DB config: {'host': 'localhost','port': 5433,'database': 'vectordb','user': 'postgres','password': 'postgres'}
-# ---
-# Create method that will run console chat with such steps:
-# - get user input from console
-# - retrieve context
-# - perform augmentation
-# - perform generation
-# - it should run in `while` loop (since it is console chat)
+##USER QUESTION: 
+{query}"""
 
+embeddings_client = DialEmbeddingsClient("text-embedding-3-small-1", API_KEY)
 
+completion_client = DialChatCompletionClient("gpt-4o", API_KEY)
+
+text_processor = TextProcessor(
+    embeddings_client=embeddings_client,
+    db_config={
+        'host': 'localhost',
+        'port': 5433,
+        'database': 'vectordb',
+        'user': 'postgres',
+        'password': 'postgres'
+    }
+)
+
+def main():
+    print("🎯 Microwave RAG Assistant")
+    print("="*100)
+
+    load_context = input("\nLoad context to VectorDB (y/n)? > ").strip()
+
+    if load_context.lower().strip() in ['y', 'yes']:
+        text_processor.process_text_file("task/embeddings/microwave_manual.txt", 150, 40, 1536)
+        print("="*100)
+
+    conversation = Conversation()
+    conversation.add_message(
+        Message(Role.SYSTEM, SYSTEM_PROMPT)
+    )
+
+    while True:
+        user_request = input("\n➡️ ").strip()
+
+        if user_request.lower().strip() in ['quit', 'exit']:
+            print("👋 Goodbye")
+            break
+
+        # Step 1: Retrieval
+        print(f"{'=' * 100}\n🔍 STEP 1: RETRIEVAL\n{'-' * 100}")
+        context_parts = text_processor.search(SearchMode.EUCLIDIAN_DISTANCE, user_request, 5, 0.5, 1536)
+        context = "\n\n".join(context_parts)
+
+        # Step 2: Augmentation
+        print(f"\n{'=' * 100}\n🔗 STEP 2: AUGMENTATION\n{'-' * 100}")
+        augmented_prompt = USER_PROMPT.format(query=user_request, context=context)
+        conversation.add_message(Message(Role.USER, augmented_prompt))
+        print(f"Prompt:\n{augmented_prompt}")
+
+        # Step 3: Generation
+        print(f"\n{'=' * 100}\n🤖 STEP 3: GENERATION\n{'-' * 100}")
+        ai_message = completion_client.get_completion(conversation.get_messages())
+        print(f"✅ RESPONSE:\n{ai_message.content}")
+        print("=" * 100)
+        conversation.add_message(ai_message)
+
+main()
 
 # TODO:
 #  PAY ATTENTION THAT YOU NEED TO RUN Postgres DB ON THE 5433 WITH PGVECTOR EXTENSION!
